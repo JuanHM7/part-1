e17bfb3121e30c98fbc2899766adbd10136ebc00
JuanH <juan4888.jh@gmail.com>
2024-07-21 23:04:47 -0700

    frontend way

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 let cash = 50;
 let price = 60;
-let extra = cash - price;
-let below =price - cash;
 
 if (cash > price){
-    console.log("you paid extra - here's "+ extra + " dollars change")
+    console.log(`you paid extra - here's ${cash - price}  dollars change`)
 }
 else if ( cash === price) {
     console.log ("you paid the exact amount, have a nice day")
 }
 else {
-    console.log("not enough money you still owe " + below +" dollars")
+    console.log(`not enough money - you still owe ${price - cash} dollars`)
 }
