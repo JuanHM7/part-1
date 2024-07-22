45225c6151e2164592db88d1e9d82b399d8cc6fe
JuanH <juan4888.jh@gmail.com>
2024-07-21 23:01:02 -0700

    practice

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 let cash = 50;
-let price = 50;
-let isStoreOpen = false;
-let difference = cash - price;
+let price = 60;
+let extra = cash - price;
+let below =price - cash;
 
- let result = difference >=0 && isStoreOpen ? 'give receipt' : 'do not give receipt';
-console.log(result);
-
+if (cash > price){
+    console.log("you paid extra - here's "+ extra + " dollars change")
+}
+else if ( cash === price) {
+    console.log ("you paid the exact amount, have a nice day")
+}
+else {
+    console.log("not enough money you still owe " + below +" dollars")
+}
