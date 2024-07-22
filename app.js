cb852a3f607cb069078e31033a43cad0e3299c05
JuanH <juan4888.jh@gmail.com>
2024-07-21 23:31:45 -0700

    using ampersand

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 let cash = 50;
 let price = 40;
-let difference = cash - price
-if (cash > price){
-    console.log(`you paid extra - here's ${difference}  dollars change`)
-}
-else if ( cash === price) {
-    console.log ("you paid the exact amount, have a nice day")
-}
-else {
-    console.log(`not enough money - you still owe ${difference * -1} dollars`)
+let isStoreOpen = false;
+
+if (cash >= price && !isStoreOpen) {
+    console.log ("print the receipt")
 }
