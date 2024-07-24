570c25e6199a56299cdbabc698bd9b0f984451e3
JuanH <juan4888.jh@gmail.com>
2024-07-23 22:54:13 -0700

    my way to solve the for exercise

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-let count = 1;
+// // let count = 1;
 
-while (count <=100) {
-    console.log(count);
-    count = count +1;
+// while (count <=100) {
+//     console.log(count);
+//     count = count +1;
+// }
+
+for (let i = 1; i <=20;  i++){
+    if (i%3 == 0 && i%5 == 0){
+        console.log("Frontend Simplified")
+    }
+    else if (i%3 == 0){
+        console.log("Frontend")
+    }
+    else if(i%5 == 0){
+        console.log("Simplifed")
+    }
+    console.log(i)
 }
+
+
