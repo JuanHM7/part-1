a0c95b5ba25fd12a70d4fcc6829daaf720b70147
JuanH <juan4888.jh@gmail.com>
2024-07-23 23:03:42 -0700

    frorntend correct answer

--- app.js
+++ app.js
@@ -6,16 +6,20 @@
 // }
 
 for (let i = 1; i <=20;  i++){
-    if (i%3 == 0 && i%5 == 0){
-        console.log("Frontend Simplified")
+    
+    if (i%3 === 0 && i%5 === 0){
+        console.log(`${i} -> Frontend Simplified`)
     }
-    else if (i%3 == 0){
-        console.log("Frontend")
+    
+    else if (i%3 === 0){
+        console.log(`${i} -> Frontend`)
     }
-    else if(i%5 == 0){
-        console.log("Simplifed")
+    else if(i%5 === 0){
+        console.log(`${i} -> Simplified`)
     }
-    console.log(i)
+    else{
+        console.log(i)    }
+    
 }
 
 
