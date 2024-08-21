8fd7cc23c5edf800631ccae1706d7538553c87f7
JuanH <juan4888.jh@gmail.com>
2024-08-20 22:59:25 -0700

    frontend simplified way. i made the mistake o using less or equal rather than just less

--- app.js
+++ app.js
@@ -6,12 +6,12 @@
 // }
 
 
-let g = ['A+', 'A', 'FAIL', 'B'];
+let g = ['A+','A','FAIL','B'];
 let g2 = [];
-for(i=0; i<=g.length;i++){
-    if(g[i] != "FAIL"){
-        g2.push(g[i])
+for(i=0; i<g.length;++i){
+    if(g[i] !== 'FAIL'){
+        g2.push(g[i]);
         
     }
 }
-console.log(g2)
+console.log(g2);
