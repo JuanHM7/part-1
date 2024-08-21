b4fcbf9d8d5abeef4d983eea3daf4fc969966465
JuanH <juan4888.jh@gmail.com>
2024-08-20 22:20:16 -0700

    array exercise my way wirhout filter.

--- app.js
+++ app.js
@@ -6,16 +6,12 @@
 // }
 
 
-let grades1 = ['A+', 'A', "FAIL"]
-let grades2 = ['Fail', 'Fail', 'B' ]
-let grades3 = ['Fail'];
-
-
-let grades11 = grades1.filter(element => element.length <= 2 )
-console.log(grades11)
-
-let grades22 = grades2.filter(element => element.length <= 2 )
-console.log(grades22)
-
-let grades33 = grades3.filter(element => element.length <= 2 )
-console.log(grades33)
+let g = ['A+', 'A', 'FAIL', 'B'];
+let g2 = [];
+for(i=0; i<=g.length;i++){
+    if(g[i] != "FAIL"){
+        g2.push(g[i])
+        
+    }
+}
+console.log(g2)
