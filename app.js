7949d58f55da5fcb36c72ccb8ed413b51f0f98e6
JuanH <juan4888.jh@gmail.com>
2024-10-07 23:00:55 -0700

    objects lesson

--- app.js
+++ app.js
@@ -1,16 +1,57 @@
-// // let count = 1;
+let users = [
+{
+   username: 'David',
+   email: 'david@frontendsimplified.com',
+   password: 'test1',
+   subscriptionStatus: 'VIP',
+   DiscordId: 'David Bragg#0001',
+   lessonCompleted: [0,1],
 
-// while (count <=100) {
-//     console.log(count);
-//     count = count +1;
-// }
+},
+{
+   username: 'Mitri',
+   email: 'Mitri@frontendsimplified.com',
+   password: 'test2',
+   subscriptionStatus: 'VIP',
+   discordId: 'Mitri#0001',
+   lessonsCompleted: [0,1,2,3],
 
-let dollars = [1 , 5 , 10, 3]
-let cents= [];
+},
 
-for(i=0;i<4;i++){
-   cents.push(dollars[i]*100)
+]
+
+function login(email, password){
+   for(let i = 0; i <users.length;++i){
+      if(users[i].email === email){
+         console.log(users[i]);
+         if(users[i].password === password){
+            console.log('log the user in - the details are correct')
+         }
+         else{
+            console.log('password is incorrect - try again')
+         }
+         return;
+      }
+      console.log('could not find an email that matches')
+   
+  
+   }
 }
 
-console.log(cents);
+login('Mitri@frontendsimplified.com','test2')
 
+
+function register(user) {
+    users.push(user);
+}
+
+register({
+   username: "zen",
+   email: "zen@frontendsimplified.com",
+   password: "zen123",
+   subscriptionStatus: "VIP",
+   discordId: "Zen#0002",
+   lessonsCompleted: [0,1]
+   });
+
+   console.log(users);
